16bcf036fe7bfeddc9262915dbf1363a8ee96f77
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-11-23 15:40:00 +0100

    split and re-organize JS and adoc files

---
+++ src/scripts/bespoke-protocol.js
@@ -0,0 +1,63 @@
+module.exports = function protocol() {
+
+  return function (deck) {
+
+    const metas = {}
+    Array.from(document.querySelectorAll('head meta')).forEach((meta) => {
+        metas[meta.getAttribute('name')] = meta.getAttribute('content')
+    })
+
+    const steps = deck.slides.map((slide, slideIdx) => {
+
+      const notes = [].slice.call(slide.querySelectorAll('aside[role="note"] p, aside[role="note"] li'))
+        .map((note) => note.textContent)
+        .join('\n')
+
+      if (slide.bullets.length > 0) {
+        return slide.bullets.map((b, bulletIdx) => {
+          return {
+            cursor: String(slideIdx) + '.' + String(bulletIdx),
+            states: [],
+            notes,
+          }
+        })
+      }
+
+      return {
+        cursor: String(slideIdx),
+        states: [],
+        notes,
+      }
+    })
+
+    const details = {
+      title: document.title || '',
+      authors: metas.author || '',
+      description: metas.description || '',
+      vendor: 'bespoke.js',
+      steps,
+      ratios: ['16/9'],
+      themes: ['default'],
+    }
+
+    window.addEventListener('message', ({ source, data: { command, commandArgs } }) => {
+
+      switch (command) {
+
+        case 'get-slide-deck-details':
+          source.postMessage({ event: 'slide-deck-details', eventData: { details } }, '*')
+          break;
+
+        case 'go-to-step':
+          const { cursor } = commandArgs
+          const [slideIdx, subslideIdx] = cursor.split('.')
+          deck.slide(Number(slideIdx))
+          deck.activateBullet(Number(slideIdx), Number(subslideIdx))
+          break;
+
+        default:
+          console.debug(`unknown protocol command ${command} with args`, commandArgs)
+      }
+    })
+  }
+}
