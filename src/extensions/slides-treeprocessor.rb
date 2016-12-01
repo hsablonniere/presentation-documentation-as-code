fea19ba7304147c6afae3df56a4305f7dc750d85
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-12-02 00:35:41 +0100

    --wip--

--- src/extensions/slides-treeprocessor.rb
+++ src/extensions/slides-treeprocessor.rb
@@ -7,64 +7,28 @@ Extensions.register do
   treeprocessor do
     process do |doc|
 
-      slides = doc.find_by :role => 'SLIDE'
-
-      doc.blocks.clear
-
-      slides.each do |slide|
-
-        # puts slide
-
-        section = Section.new doc, 1
-        section.title = "!"
-        section.id = slide.id
-
-        # this seems to be needed but why?
-        section.set_attr "id", ""
-
-        if slide.context === :open
-          section.roles.concat slide.roles
-          section.blocks.concat slide.blocks
+      slides = (doc.find_by role: 'SLIDE').map {|slide|
+        slide.parent.blocks.delete slide
+        sect = Asciidoctor::Section.new doc, 1, false
+        sect.title = '!'
+        sect.id = slide.id
+        slide.id = nil
+        if slide.blocks?
+          slide.blocks.each do |child|
+            sect << (slide.blocks.delete child)
+            child.remove_role 'SLIDE'
+            child.parent = sect
+          end
         else
-          slide.id = nil
+          sect << slide
           slide.remove_role 'SLIDE'
-          section.blocks << slide
+          slide.parent = sect
         end
-
-        doc.blocks << section
-
-      end
-
-      # puts 'blocks'
-      # pp doc
-
-
-
-      # section = Section.new doc
-      # section.level = 1
-      # section.title = "!"
-      # section.generate_id
-      # puts slides
-
-
-      # , "", {
-      #   :title => '!',
-      #   :level => 1
-      # }
-      # section.set_attr "title", '!'
-      # section.set_attr "level", 1
-
-      # pp section
-
-      # doc.blocks << section
-      # pp doc.blocks[0]
-      # puts doc.blocks[0].id
-      # puts doc.blocks[1].id
-      # puts doc.blocks[2].attributes
-      # puts doc.blocks[3].attributes
-      # puts doc.blocks[4].attributes
-      # doc.blocks.concat slides
-      doc
+        sect
+      }
+      doc.blocks.replace slides
+      nil
+      
     end
   end
 end
